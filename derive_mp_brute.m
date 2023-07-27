1b151a343ff8ef4eb04e9f4592457e9486e6e381
HUGO RYU <skryu9607@kaist.ac.kr>
2023-07-27 18:10:45 +0900

    kk

---
+++ derive_mp_brute.m
@@ -0,0 +1,19 @@
+function [mp_idx] = derive_mp_brute(Trajectory,n)
+%DERIVE_MP Summary of this function goes here
+%   n : the number of MPs
+mp_idx = [];
+for i = 1 : size(Trajectory,1)
+    a = Trajectory(i,:);
+    for j = 1 : size(Trajectory,2)-1
+        if j == 1
+            b(j) = a(j+1) - 1;
+        else
+            b(j) = a(j+1) - (((a(j)-1)*n)+1);
+        end
+    end
+    mp_idx = [mp_idx;b];
+end
+
+       
+end
+
